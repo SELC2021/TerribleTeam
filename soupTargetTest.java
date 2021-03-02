a9b7163827073fdcbb18d40d3a5c538d57f5a06c
Evan Brummer <ebrummer@iastate.edu>
2021-03-02 14:44:33 -0600

    Create soupTargetTest.java

---
+++ soupTargetTest.java
@@ -0,0 +1,43 @@
+package testWeb;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class soupInputTest {
+	public static void main(String args[]){
+		print("running...");
+		Document document;
+		
+		Scanner sc = new Scanner(System.in);
+		String str = new String();
+		
+		System.out.print("Enter Item URL From Amazon: ");
+		str = sc.next();
+		
+		try {
+			//Get Document object after parsing the html from given url.
+			document = Jsoup.connect(str).get();
+			
+			String title = document.title(); //Get title
+			print("  Title: " + title); //Print title.
+			
+			Elements price = document.select("span#price_inside_buybox"); //get price
+			for (int i=0; i < price.size(); i++) {
+				print(price.get(i).text());
+				}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		sc.close();
+		print("done");
+	}
+
+	public static void print(String string) {
+		System.out.println(string);
+	}
+}
