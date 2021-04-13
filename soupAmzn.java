2d6ddfa7649a9cf17ea096dc171fe567d4c7ba70
Evan Brummer <ebrummer@iastate.edu>
2021-04-13 14:50:43 -0500

    changed price check stuff
    lets goooo

--- soupAmzn.java
+++ soupAmzn.java
@@ -1,91 +1,97 @@
-package SEProjMar9;
+package testing;
 
 import java.io.IOException;
-import java.util.Scanner;
+
+
+import javax.swing.JOptionPane;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
-/**
- * STEP 1: make a new soupAmzn() object TWO COMMANDS: getUrl() and getDocPrice()
- * 
+import java.awt.Desktop;
+import java.net.URI;
+import java.net.URISyntaxException;
+/** 
+ * STEP 1: make a new soupAmzn() object
+ * TWO COMMANDS: getUrl() and getDocPrice()
  * @author Evan
  *
  */
 public class soupAmzn {
 	private Document document;
-	private Scanner sc;
 	private String asinID;
 	private String url;
 	private String currPrice;
 	private String prevPrice;
 	private String cartLink;
-
-	private double dbCurr;
-	private double dbPrev;
-
-	double dollarToDouble(String str) {
-		if (str == null) {
-			return -1;
-		}
-		
-		String stres = new String();
-		double result;
-
-		for (int i = 1; i < str.length(); i++) {
-			stres += str.charAt(i);
-		}
-
-		result = Double.parseDouble(stres);
-
-		return result;
-	}
-
+	
 	soupAmzn() {
-		sc = new Scanner(System.in);
-		asinID = new String();
+		
 		url = new String();
+		currPrice = null;
+		prevPrice = null;
 	}
-
+	
 	void getURL() {
-		System.out.print("Enter Item ASIN ID From Amazon: ");
-		asinID = sc.next();
-		url = "https://www.amazon.com/gp/product/" + asinID;
+		String asinID = JOptionPane.showInputDialog(null, "Enter the item's ASIN ID here:");
+		
+	
+		
+		System.out.println("Your ASIN ID is " + asinID);
+		
+		url = "https://www.amazon.com/gp/product/" + asinID; 
 		cartLink = "https://www.amazon.com/gp/aws/cart/add.html?&ASIN.1=" + asinID + "&Quantity.1=1";
-		// url = sc.next();
+		
 	}
-
-	double getDocPrice() {
+	
+	void getDocPrice() throws IOException, URISyntaxException {
 		try {
 			document = Jsoup.connect(url).get();
-		} catch (IOException e) {
+		} catch (IOException e) { 
 			// Auto-generated catch block
 			e.printStackTrace();
 		}
-		Elements price = document.select("span#price_inside_buybox"); // get price
-
-		for (int i = 0; i < price.size(); i++) {
+		Elements price = document.select("span#price_inside_buybox"); //get price
+		
+		
+		for (int i=0; i < price.size(); i++) {			
+			//prevPrice = currPrice;
 			prevPrice = currPrice;
 			currPrice = price.get(i).text();
-			
-			if (price.size() != 0) {
-				dbPrev = dollarToDouble(prevPrice);
-				dbCurr = dollarToDouble(currPrice);
-			}
-
-			if (currPrice.equals(prevPrice) || (i == 0)) {
+			 
+			if (currPrice.equals(prevPrice) || prevPrice == null) {
 				System.out.print("Price has not changed: ");
 				System.out.println(currPrice);
-				System.out.println("----LINK----\n" + cartLink + "\n");
-			} else {
+				
+			} 
+			else {
+				
+				
 				System.out.println("");
 				System.out.print("PRICE HAS CHANGED: ");
 				System.out.println(currPrice);
-				System.out.println("----LINK----\n" + cartLink + "\n");
-			}
+				System.out.println("Link: " + cartLink);
+				System.out.println("");
+				String end = JOptionPane.showInputDialog(null, "Your price has changed to " + currPrice + ". would you like to buy? (y/n)");
+				
+				if (end.equals("y")) {
+					try {
+						System.out.println("Opening webpage...");
+						  Desktop desktop = java.awt.Desktop.getDesktop();
+						  URI oURL = new URI(cartLink);
+						  desktop.browse(oURL);
+						} 
+					catch (Exception e) {
+						  e.printStackTrace();
+						}
+			
+				}
+				
+			}			
 		}
-		return dbCurr;
+		
 	}
 
+	
 }
